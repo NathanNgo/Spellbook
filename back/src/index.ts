5d067d4e8923d3df80571d801a2ea1fa9a3aec1f
= <24886321+itsjustmustafa@users.noreply.github.com>
2025-01-21 03:00:03 +1100

    formatting changes

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,29 +1,32 @@
-import express from 'express'
-import sqlite3 from "sqlite3"
-import cors from 'cors'
-import type { Request, Response } from 'express'
+import express from "express";
+import sqlite3 from "sqlite3";
+import cors from "cors";
+import type { Request, Response } from "express";
 
-const DATABASE_FILE_PATH = "database/spellbook.db"
+const DATABASE_FILE_PATH = "database/spellbook.db";
 
-const app = express()
-const port = 3000
-app.use(cors())
+const app = express();
+const port = 3000;
+app.use(cors());
 
-const database = new sqlite3.Database(DATABASE_FILE_PATH,  (err) => {
-  if (err) {
-    console.error("Could not open database:", err.message);
-  } else {
-    console.log("Connected to SQLite database.");
-  }
+const database = new sqlite3.Database(DATABASE_FILE_PATH, (err) => {
+    if (err) {
+        console.error("Could not open database:", err.message);
+    } else {
+        console.log("Connected to SQLite database.");
+    }
 });
 
-app.get('/', (_: Request, res: Response) => {
-  database.all("SELECT * FROM d20pfsrd WHERE name = 'Fireball' OR name = 'Magic Missile' OR name = 'Wish';", (_, rows) => {
-    console.log(rows)
-    res.send(rows)
-  })
-})
+app.get("/", (_: Request, res: Response) => {
+    database.all(
+        "SELECT * FROM d20pfsrd WHERE name = 'Fireball' OR name = 'Magic Missile' OR name = 'Wish' OR name = 'Grease' OR name = 'Charm Person';",
+        (_, rows) => {
+            console.log(rows);
+            res.send(rows);
+        }
+    );
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on localhost:${port}`)
-})
+    console.log(`Example app listening on localhost:${port}`);
+});

a41a84d4351f7bac07846ce2a305e20bc842ea83
= <24886321+itsjustmustafa@users.noreply.github.com>
2025-01-22 01:59:03 +1100

    added dark colour scheme and check preference in css

--- front/src/components/searchBar/SearchBar.tsx
+++ front/src/components/searchBar/SearchBar.tsx
@@ -3,7 +3,11 @@ import style from "components/searchBar/SearchBar.module.css";
 function SearchBar() {
     return (
         <div className={style.searchbar}>
-            <input type="text" placeholder="Search current spellbook" />
+            <input
+                type="text"
+                className={style.searchbarInput}
+                placeholder="Search current spellbook"
+            />
             <button className="symbol">search</button>
         </div>
     );

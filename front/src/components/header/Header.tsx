7d1264ad3507ae614f0d0d118cafa2fec6dbc312
= <24886321+itsjustmustafa@users.noreply.github.com>
2025-01-25 20:27:12 +1100

    header takes in a function to toggle the menu

--- front/src/components/header/Header.tsx
+++ front/src/components/header/Header.tsx
@@ -2,13 +2,14 @@ import styles from "components/header/Header.module.css";
 
 type Props = {
     characterName: string;
+    toggleMenu: () => void;
 };
 
-function Header({ characterName }: Props) {
+function Header({ characterName, toggleMenu }: Props) {
     return (
         <div className={styles.stickyHeaderContainer}>
             <header className={styles.mainHeader}>
-                <button className={styles.menuButton}>
+                <button className={styles.menuButton} onClick={toggleMenu}>
                     <span className="symbol">menu</span>
                 </button>
                 <h1 className={styles.pageTitle}>Spellbook</h1>

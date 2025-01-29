f88adc4a60815a333fc185fcc790b4841d30ce9d
Nathan Ngo <nathanj238@gmail.com>
2025-01-29 22:48:14 +1100

    Changed session name

--- dev.sh
+++ dev.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # This prevents recursive sessions as tmux sessions don't die when the terminal is closed.
-tmux kill-session -t dev
+tmux kill-session -t spellbookdev
 
-tmux new-session -d -s dev
+tmux new-session -d -s spellbookdev
 
 tmux split-window -h
 
-tmux send-keys -t dev:0.0 "cd back/ && yarn start" C-m
+tmux send-keys -t spellbookdev:0.0 "cd back/ && yarn start" C-m
 
-tmux send-keys -t dev:0.1 "cd front/ && yarn dev" C-m
+tmux send-keys -t spellbookdev:0.1 "cd front/ && yarn dev" C-m
 
-tmux attach -t dev
+tmux attach -t spellbookdev

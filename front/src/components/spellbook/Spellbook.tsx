11438c38d91c86fca368ecfb2a732e241703e607
= <24886321+itsjustmustafa@users.noreply.github.com>
2025-02-02 02:49:43 +1100

    add/changed props to spellbook, specifying its only displayed spells, and including the total spell count

--- front/src/components/spellbook/Spellbook.tsx
+++ front/src/components/spellbook/Spellbook.tsx
@@ -1,9 +1,11 @@
+import Message from "components/message/Message";
 import SpellTable from "components/spellTable/SpellTable";
 import styles from "components/spellbook/Spellbook.module.css";
 import type { Spells } from "schemas";
 
 type Props = {
-    spells: Spells;
+    spellsToDisplay: Spells;
+    totalSpellCount: number;
 };
 
 const LEVEL_TITLES = [
@@ -21,20 +23,30 @@ const LEVEL_TITLES = [
 
 const UNCATEGORISED_LEVEL = -1;
 
-function Spellbook({ spells }: Props) {
+function Spellbook({ spellsToDisplay, totalSpellCount }: Props) {
+    if (totalSpellCount === 0) {
+        return <Message>Spellbook is empty</Message>;
+    }
+
+    if (spellsToDisplay.length === 0) {
+        return <Message>No results found</Message>;
+    }
+
     return (
         <div className={styles.spellBook}>
             {Array.from(Array(LEVEL_TITLES.length), (_, level) => {
                 return (
                     <SpellTable
-                        spells={spells.filter((spell) => spell.sor === level)}
+                        spells={spellsToDisplay.filter(
+                            (spell) => spell.sor === level
+                        )}
                         title={LEVEL_TITLES[level]}
                         key={level}
                     />
                 );
             })}
             <SpellTable
-                spells={spells.filter((spell) => spell.sor === null)}
+                spells={spellsToDisplay.filter((spell) => spell.sor === null)}
                 title="Uncategorised"
                 key={UNCATEGORISED_LEVEL}
             />

d16ba72b23f5b1c9c4b30c31fccb6c4a93c2cf8a
= <24886321+itsjustmustafa@users.noreply.github.com>
2025-02-28 00:11:25 +1100

    Now checking if the spells are loaded before setting the spells in local storage

--- front/src/components/spellbookContainer/SpellbookContainer.tsx
+++ front/src/components/spellbookContainer/SpellbookContainer.tsx
@@ -106,7 +106,9 @@ function SpellbookContainer({
 
     useEffect(() => {
         const spellNames = spells.map((spell) => spell.name);
-        localStorage.setItem(SPELLNAMES_KEY, JSON.stringify(spellNames));
+        if (spellsLoaded) {
+            localStorage.setItem(SPELLNAMES_KEY, JSON.stringify(spellNames));
+        }
     }, [spells]);
 
     function handleCloseDrawer() {

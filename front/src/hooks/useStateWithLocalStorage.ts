7a1c5e59bdbd38aef3674f68fda9b8a3bdf74cb1
= <24886321+itsjustmustafa@users.noreply.github.com>
2025-03-12 00:47:22 +1100

    cleanup with setting initial state value in local storage hook

--- front/src/hooks/useStateWithLocalStorage.ts
+++ front/src/hooks/useStateWithLocalStorage.ts
@@ -4,18 +4,19 @@ function useStateWithLocalStorage<StateType>(
     key: string,
     defaultValue: StateType
 ) {
-    const initialValue = localStorage.getItem(key);
-    const loadedFromStorage = useRef<boolean>(initialValue !== null);
+    const loadedFromStorage = useRef<boolean>(false);
     const [state, setState] = useState<StateType>(() => {
+        const initialValue = localStorage.getItem(key);
         if (initialValue !== null) {
             try {
                 loadedFromStorage.current = true;
                 return JSON.parse(initialValue) as StateType;
             } catch {
-                loadedFromStorage.current = false;
-                return defaultValue;
+                // Can't parse local storage value as json, invalid storage
+                // Should just give back defaultValue
             }
         }
+        loadedFromStorage.current = false;
         return defaultValue;
     });
 

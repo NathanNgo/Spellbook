47b7b60d167c66ff012834c79b7d253eee67a159
Nathan Ngo <nathanj238@gmail.com>
2025-03-19 19:23:51 +1100

    Updated to use production url on build

--- front/src/urls.tsx
+++ front/src/urls.tsx
@@ -1,5 +1,12 @@
 export const BACKEND_PORT = "3000";
-export const BACKEND_BASE_URL = `http://localhost:${BACKEND_PORT}`;
+export const DEVELOPMENT_BACKEND_BASE_URL = `http://localhost:${BACKEND_PORT}`;
+export const PRODUCTION_BACKEND_BASE_URL =
+    "https://q3tip87tp2.ap-southeast-2.awsapprunner.com/";
+
+export const BACKEND_BASE_URL = import.meta.env.DEV
+    ? DEVELOPMENT_BACKEND_BASE_URL
+    : PRODUCTION_BACKEND_BASE_URL;
+
 export const SPELL_SUMMARIES_ENDPOINT = new URL(
     "spellSummaries",
     BACKEND_BASE_URL
